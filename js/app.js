6da60d326badad82829e1ab7ba92539663f6366d
Faddal Ibrahim <41176161+faddalibrahim@users.noreply.github.com>
2020-08-18 13:24:43 +0000

    picking words from api

---
+++ js/app.js
@@ -0,0 +1,39 @@
+import BavisMeacon from './BavisMeacon.js';
+import { $, $$, $id } from './binds.js';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    let WORDS;
+
+    fetch('https://api.quotable.io/quotes')
+    .then(res => res.json())
+    .then(({results}) => {
+        WORDS = results.map(({content}) => content);
+        console.log(WORDS)
+        WORDS.sort(() => 0.5 - Math.random())
+        console.log(WORDS)
+        init()
+    })
+
+    function init(){
+        const COUNT_DOWN = $('#controls .count-down'),
+              CONTROLS   = $id('controls'),
+              MARQUEE    = $('#word-stream marquee'),
+              FORM       = $id('form'),
+              SETTINGS_FORM       = $id('settings-form');
+  
+        
+        const newGame = new BavisMeacon(COUNT_DOWN, CONTROLS, MARQUEE, FORM, WORDS, SETTINGS_FORM, $, $$);
+        
+        CONTROLS.addEventListener('click', newGame.action);
+        SETTINGS_FORM.addEventListener('submit', newGame.applySettings);
+        FORM.addEventListener('submit', newGame.nextWord);
+    }
+
+  
+
+    
+   
+
+
+})
